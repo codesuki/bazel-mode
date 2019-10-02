13a8efc7b388b3ac45dd981898953bd98dd1b3d3
Neri Marschik <codesuki@users.noreply.github.com>
2019-10-02 12:33:52 +0900

    Add font lock keywords for some builtins

--- bazel-mode.el
+++ bazel-mode.el
@@ -1,10 +1,10 @@
 ;;; bazel-mode.el --- A major mode for editing Bazel files
 
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Author: Neri Marschik
 ;; Url: https://github.com/codesuki/bazel-mode
 ;; Keywords: languages, bazel
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "26"))
 
 ;;; Commentary:
 ;;
@@ -51,8 +51,7 @@
     (with-current-buffer result-buffer (erase-buffer))
     (if (zerop (call-process-region (point-min) (point-max) bazel-format-command nil result-buffer nil))
         (progn
-          (with-current-buffer current-buffer (delete-region (point-min) (point-max)))
-          (with-current-buffer current-buffer (insert-buffer-substring result-buffer))
+          (with-current-buffer current-buffer (replace-buffer-contents result-buffer))
           (goto-char oldpoint))
       (message "bazel-format failed"))
     (kill-buffer result-buffer)))
@@ -62,16 +61,103 @@
     (define-key map "\C-c\C-f" 'bazel-format)
     map))
 
+(defconst bazel-mode--font-lock-keywords
+  `(
+       ;; attr
+    (,(regexp-opt '("attr.bool" "attr.int" "attr.int_list" "attr.label" "attr.label_keyed_string_dict"
+                    "attr.label_list" "attr.license" "attr.output" "attr.output_list" "attr.string"
+                    "attr.string_dict" "attr.string_list" "attr.string_list_dict")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    ;; See https://github.com/bazelbuild/starlark/blob/master/spec.md.
+    (,(regexp-opt '("and" "else" "load" "break" "for" "not" "continue" "if" "or" "def" "in" "pass" "elif" "return")
+                  'symbols)
+     . 'font-lock-keyword-face)
+
+    (,(regexp-opt '("True" "False" "None")
+                  'symbols)
+     . 'font-lock-constant-face)
+
+    (,(regexp-opt '("any" "all" "bool" "dict" "dir" "enumerate"
+                    "getattr" "hasattr" "hash" "int" "len" "list"
+                    "max" "min" "print" "range" "repr" "reversed"
+                    "sorted" "str" "tuple" "type" "zip")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    ;; string
+    (,(regexp-opt '("capitalize" "count" "elems" "endswith" "find" "format" "index"
+                    "isalnum" "isalpha" "isdigit" "islower" "isspace" "istitle" "isupper"
+                    "join" "lower" "lstrip" "partition" "replace"
+                    "rfind" "rindex" "rpartition" "rsplit" "rstrip"
+                    "split" "splitlines" "startswith" "strip" "title" "upper")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    ;; dict
+    ;; (,(regexp-opt '("clear" "get" "items" "keys" "pop" "popitem" "setdefault" "update" "values")
+    ;;               'symbols)
+    ;;  . 'font-lock-builtin-face)
+
+    ;; list
+    ;; (,(regexp-opt '("append" "clear" "extend" "index" "insert" "pop" "remove")
+    ;;               'symbols)
+    ;;  . 'font-lock-builtin-face)
+
+    ;; bazel specific
+    (,(regexp-opt '("PACKAGE_NAME" "REPOSITORY_NAME")
+                  'symbols)
+     . 'font-lock-constant-face)
+
+    (,(regexp-opt '("all" "analysis_test_transition" "aspect" "bind" "configuration_field" "depset" "existing_rules" "fail"
+                    "fail" "provider" "register_execution_platforms" "register_toolchains" "repository_rule"
+                    "rule" "select" "workspace")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    (,(regexp-opt '("existing_rule" "exports_files" "glob" "package_group" "package_name" "repository_name")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    (,(regexp-opt '("AppleDebugOutputs" "AppleDylibBinary" "AppleDynamicFramework" "AppleExecutableBinary"
+                    "AppleLoadableBundleBinary" "AppleStaticLibrary" "CcInfo" "CcSkylarkApiProvider"
+                    "CcToolchainConfigInfo" "CcToolchainInfo" "CompilationContext" "ConstraintCollection"
+                    "ConstraintSettingInfo" "ConstraintValueInfo" "DefaultInfo" "FeatureFlagInfo"
+                    "file_provider" "FilesToRunProvider" "GeneratedExtensionRegistryProvider" "InstrumentedFilesInfo"
+                    "java" "java_compilation_info" "java_output_jars" "JavaCcLinkParamsInfo""JavaInfo" "JavaRuntimeInfo"
+                    "JavaToolchainInfo" "ObjcProvider" "OutputGroupInfo" "PlatformInfo" "ProguardSpecProvider" "ProtoInfo"
+                    "ProtoRegistryProvider" "PyInfo" "PyRuntimeInfo" "TemplateVariableInfo" "ToolchainInfo" "ToolchainTypeInfo"
+                    "XcodeProperties" "XcodeVersionConfig")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+
+
+    ;; rules_go
+    (,(regexp-opt '("go_image" "go_test" "go_binary" "go_library")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    ;; rules_docker
+    (,(regexp-opt '("container_image" "passwd_entry" "passwd_file" "passwd_file" "passwd_tar")
+                  'symbols)
+     . 'font-lock-builtin-face)
+
+    ;; bazel_tools
+    (,(regexp-opt '("git_repository" "http_archive" "pkg_tar")
+                  'symbols)
+     . 'font-lock-builtin-face)))
+
 ;;;###autoload
 (define-derived-mode bazel-mode python-mode "Bazel"
   "Major mode for editing Bazel files."
   :group 'bazel
 
   (setq-local comment-use-syntax t)
-  (setq-local comment-start "#")
-  (setq-local comment-end "")
-  (setq-local indent-tabs-mode nil)
-  
+
+  (setq-local font-lock-defaults (list bazel-mode--font-lock-keywords))
+
   ;; Add imenu support
   ;; Replace python-imenu-create-index with the default one
   (setq-local imenu-create-index-function #'imenu-default-create-index-function)
